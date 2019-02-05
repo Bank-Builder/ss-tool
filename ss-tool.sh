8bb11e844e8b3efc6e4870cab9fe59eadd99d744
Andrew Turpin <andrewt@grindrodbank.co.za>
2019-02-05 23:56:37 +0200

    Bumped version to 0.2

--- ss-tool.sh
+++ ss-tool.sh
@@ -6,29 +6,31 @@
 #-----------------------------------------------------------------------
 
 
-_version="0.1"
+_version="0.2"
 
 function displayHelp(){
- echo "Usage ss-tool [OPTION]... [FILE]";
+ echo "Usage: ss-tool [OPTION]...";
  echo "   Clones the repositories of multiple microservices as per the ss.conf file";
  echo "   and drops & recreates these discrete schemas into a single canonical database";
  echo "   which it then round-trips back into the canonical repository";
  echo " ";
  echo "  OPTIONS:";
- echo "    -f, --file      supply file name of the ss.conf";
- echo "                    the default is ss.conf in the current path";
- echo "    -v, --verbose   display vebose details and progress bar.  Works with -f option only.";
- echo "    -c, --cleanup   removes the current directory & all sub-directories when done, leaving no trace.";
+ echo "    -f, --file      supply optional file name of alternative ss-tool.conf file";
+ echo "    -s, --silent    does not display vebose details";
+ echo "    -c, --cleanup   removes all git cloned sub-directories when done";
  echo "        --help      display this help and exit";
  echo "        --version   display version and exit";
- echo "Usage ss [OPTION]... [FILE]";
+ echo "";
  echo "";
  echo "  EXAMPLE(s):";
- echo "      ss-tool -f [ss-tool.conf]";
+ echo "      ss-tool --cleanup";
+ echo "           will remove all the sub-folders of cloned repositories after it has completed executing.";
  echo "";
  echo "      ss-tool.conf:";
  echo "          [canonical-database] section";
- echo "          [<microservice>] section(s) giving details of repo with flyway sql to build micro-service schemas";
+ echo "          [<microservice>] section(s) giving details of repo with amongst other settings, ";
+ echo "                           the source path to the FlyWay SQL scripts to";
+ echo "                           add micro-service schemas to canonical-database";
  echo "";
 }
 
@@ -39,62 +41,67 @@ function displayVersion(){
  echo "";
 }
 
-function processConfig(){
-    Config="$1"
-    
-    IFS=$'\n'
-    total=`cat $Config | wc -l`
-    number=1
-    
-    for line in $(cat $Config)
-    do
-        confLabel=$( echo "$line" |cut -d'=' -f1 );
-        confValue=$( echo "$line" |cut -d'=' -f2 ); 
+function evaluate(){
+ #if _verbose="0" then redirect stdout to null else show on screen
+ eval "$1"
+}
 
-        if [[ ${confLabel:0:1} != "#" ]]; then #not a comment
-        
-            tmp=${confLabel#*[}   # remove prefix ending in "["
-            folder=${tmp%]*}   # remove suffix starting with "]"
-        
-            # exit if dangerous folder names in config
-            if [[ "~/." == *"$folder"* ]]; then 
-                printf "Dangerous config! \n[$folder] is not allowed in .conf\n"; exit; 
-            fi;
-            
-            # clone folders if dont exist
-            if [ "$confLabel" = "[$folder]" ]; then 
-                if [ -d "$folder" ]; then
-                    echo "Pulling $folder";
-                    eval "cd $folder"; eval "git pull"; eval "cd ..";
-                    clone="";
-                else 
-                    clone=$folder; 
-                fi;
-            fi;
-            
-            # clone the folders as per the config file if folder does not exist
-            if [ "$confLabel" = "source" ] && [ "$clone" != "" ]; then eval $confValue; fi;
-  
-            # spin up the postgres docker container
-            # connect and create the database as per conf file
-            # drop the non-canonical schema's from canonical db
-            # execute the schema .sql flyways against the canonical db
-            # sqldump the canonical
-            # overwrite the canonical repo with revised sql
-            # git push -u
-            # stop amd rmi container
-            # cd .. and rm -rf schema-tool dir if param = --remove-when-done
-            
-             
-       fi;   
-                
-    done
+function processConfig(){
+ Config="$1"
+ Verbose="$2"
+ IFS=$'\n'
+
+ for line in $(cat $Config)
+ do
+   confLabel=$( echo "$line" |cut -d'=' -f1 );
+   confValue=$( echo "$line" |cut -d'=' -f2 ); 
+
+   if [[ ${confLabel:0:1} != "#" ]]; then #not a comment
+
+    tmp=${confLabel#*[}   # remove prefix ending in "["
+    folder=${tmp%]*}   # remove suffix starting with "]"
+
+    # exit if dangerous folder names in config
+    if [[ "~/." == *"$folder"* ]]; then 
+        printf "Dangerous config! \n[$folder] is not allowed in .conf\n"; exit; 
+    fi;
+    
+    # clone folders if dont exist
+    if [ "$confLabel" = "[$folder]" ]; then 
+        if [ -d "$folder" ]; then
+            echo "Pulling $folder";
+            evaluate "cd $folder"; evaluate "git pull"; evaluate "cd ..";
+            clone="";
+        else 
+            clone=$folder; 
+        fi;
+    fi;
+    
+    # clone the folders as per the config file if folder does not exist
+    if [ "$confLabel" = "source" ] && [ "$clone" != "" ]; then evaluate $confValue; fi;
+
+    ## TODO ##
+    # spin up the postgres docker container
+    # connect and create the database as per conf file
+    # drop the non-canonical schema's from canonical db
+    # execute the schema .sql flyways against the canonical db
+    # sqldump the canonical
+    # overwrite the canonical repo with revised sql
+    # git push -u
+    # stop amd rmi container
+    # cd .. and rm -rf schema-tool dir if param = --remove-when-done
+    
+     
+  fi;   
+           
+ done
 
 }
 
 # __Main__
-_verbose="0"
-_markdown="0"
+_verbose="1"
+_configFile="ss-tool.conf"
+
 while [[ "$#" > 0 ]]; do
     case $1 in
         --help) 
@@ -104,8 +111,8 @@ while [[ "$#" > 0 ]]; do
         -f|--file) 
             _confileFile="$2";
             shift;;
-        -v|--verbose) 
-            _verbose="1"
+        -s|--silent) 
+            _verbose="0"
             ;;
         -c|--cleanup) 
             _cleanup="1";
@@ -118,14 +125,12 @@ done
 
 if [ "$_verbose" = "1" ]; then 
     _title="ss-tool ver $_version";
-    if [ "$_markdown" = "1" ]; then _title="# $_title";
-    else _title="$_title\n======================";
-    fi;
+    _title="$_title\n======================";
     echo -e $_title
 fi
 
 
-if [ -n "$_configFile" ]; then processConfig $_configFile $_verbose $_markdown;exit 0; fi;
+if [ -n "$_configFile" ]; then processConfig $_configFile $_verbose;exit 0; fi;
 
 echo "Try ss-tool --help for help";
 #FINIS
